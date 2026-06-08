68ecd0dbef0a92e691a260c3404bbfdfc2a7ebdb
notprayasmitra <notprayasmitra@proton.me>
2026-06-08 12:03:37 +0530

    Added Update and Delete features of the CRUD app

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
@@ -8,11 +9,35 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-mongoose.connect("mongodb+srv://prayasmitra:krrish3575npm@basic-crud-in-mern.mokxiio.mongodb.net/crud")
+mongoose.connect(process.env.MONGODB_URI)
 
 app.get('/', (req, res) => {
     UserModel.find({})
     .then(users => res.json(users))
+    .catch(err => res.json(err))
+})
+
+app.get('/getUser/:id', (req, res) => {
+    const id = req.params.id;
+    UserModel.findById({_id: id})
+    .then(users => res.json(users))
+    .catch(err => res.json(err))
+})
+
+app.put('/updateUser/:id', (req, res) => {
+    const id = req.params.id;
+    UserModel.findByIdAndUpdate({_id: id}, {
+        name: req.body.name, 
+        email: req.body.email, 
+        age: req.body.age})
+    .then(users => res.json(users))
+    .catch(err => res.json(err))
+})
+
+app.delete('/deleteUser/:id', (req, res) => {
+    const id = req.params.id;
+    UserModel.findByIdAndDelete({_id: id})
+    .then(res => res.json(res))
     .catch(err => res.json(err))
 })
 
